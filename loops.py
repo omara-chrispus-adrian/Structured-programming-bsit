6c2c785995a103eaf297a77e25875d68b98637fa
omara-chrispus-adrian <adrianchrispus@gmail.com>
2025-06-23 15:08:47 +0300

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,17 @@
+# for loop syntax
+
+"for count in condition logic "
+
+#EXAMPLE 1: WRITE A PROGRAM TO PRINT THE FIRST 100 EVEN NUMBERS#
+
+#OPTION 1 #
+for number in range(1,201):
+    if number%2 == 0:
+        print(number)
+
+
+
+#OPTION 2#
+for number in range(0,201,2):
+    print(number)
+
